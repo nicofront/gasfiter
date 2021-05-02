cfdd7605beb42cc9d68752602dad3de68193e412
Nico Perez <nicolasperez@multiplica.com>
2021-05-01 20:29:40 -0400

    Initial commit

---
+++ footer.php
@@ -0,0 +1,111 @@
+<footer id="footer" class="border-0 bg-dark pt-4 mt-0">
+				<div class="custom-svg-wrapper-4 pb-1">
+					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1667 581">
+						<g data-plugin-float-element data-plugin-options="{'startPos': 'top', 'speed': 0.3, 'isInsideSVG': true, 'transition': true, 'transitionDuration': 2000}">
+							<circle class="custom-svg-fill-color-secondary" fill="#4F4BFC" cx="39" cy="315" r="17"/>
+							<circle fill="#222529" cx="38.69" cy="315.31" r="15.43"/>
+						</g>
+						<g data-plugin-float-element data-plugin-options="{'startPos': 'bottom', 'speed': 0.2, 'isInsideSVG': true, 'transition': true, 'transitionDuration': 2000}">
+							<circle class="custom-svg-fill-color-secondary" fill="#5349FF" cx="257" cy="545" r="4"/>
+						</g>
+						<g data-plugin-float-element data-plugin-options="{'startPos': 'top', 'speed': 0.3, 'isInsideSVG': true, 'transition': true, 'transitionDuration': 2000}">
+							<circle fill="#333539" cx="501.5" cy="304.5" r="10.5"/>
+						</g>
+						<g data-plugin-float-element data-plugin-options="{'startPos': 'bottom', 'speed': 0.2, 'isInsideSVG': true, 'transition': true, 'transitionDuration': 2000}">
+							<circle class="custom-svg-fill-color-secondary" fill="#5349FF" cx="738" cy="36" r="6"/>
+						</g>
+						<g data-plugin-float-element data-plugin-options="{'startPos': 'top', 'speed': 0.3, 'isInsideSVG': true, 'transition': true, 'transitionDuration': 2000}">
+							<circle class="custom-svg-fill-color-secondary" fill="#5349FF" cx="1630" cy="368" r="6"/>
+						</g>
+					</svg>
+					<div class="container mt-5 mt-lg-0">
+						<div class="row">
+
+							<Div class="col-lg-12 text-center pb-3 pb-lg-0 mb-1 mb-lg-0">
+								<span class="d-block text-color-grey positive-ls-2 font-weight-normal custom-font-size-2 text-color-light opacity-6 mb-1">CONTACTO</span>
+                                                        
+								<h2 class="font-weight-bold text-color-light text-10 mb-5">Agende una Visita </h2>
+								<div class="row">
+									<div class="col-lg-12">
+										
+										<div class="row pb-5 mb-1">
+
+                                                <div class="col-lg-3 col-mb-6 mb-2">
+                                                    <div data-plugin-float-element data-plugin-options="{'startPos': 'bottom', 'speed': 0.2, 'isInsideSVG': true, 'transition': true, 'transitionDuration': 2000}">
+                                                        <a href="tel:+56984038643" class="text-4 text-color-light text-color-hover-primary opacity-5 font-weight-light mb-0">
+                                                            <img class="icon-animated" width="55" src="img/icons/icon-phone.svg" alt="" data-icon data-plugin-options="{'onlySVG': true, 'extraClass': 'svg-fill-color-secondary mb-3'}" />
+                                                            <h4 class="font-weight-bold text-color-light text-5 mb-0">Teléfono 1</h4>
+                                                            <p class="text-3 text-color-light opacity-5 font-weight-light mb-0">+56 9 8403 8643</p>
+                                                        </a>
+                                                    </div>
+                                                </div>
+
+
+                                                <div class="col-lg-3 col-mb-6 mb-2">
+                                                    <div data-plugin-float-element data-plugin-options="{'startPos': 'bottom', 'speed': 0.2, 'isInsideSVG': true, 'transition': true, 'transitionDuration': 2000}">
+                                                        <a href="tel:+56955356490" class="text-4 text-color-light text-color-hover-primary opacity-5 font-weight-light mb-0">
+                                                            <img class="icon-animated" width="55" src="img/icons/icon-phone.svg" alt="" data-icon data-plugin-options="{'onlySVG': true, 'extraClass': 'svg-fill-color-secondary mb-3'}" />
+                                                            <h4 class="font-weight-bold text-color-light text-5 mb-0">Teléfono 2</h4>
+                                                            <p class="text-3 text-color-light opacity-5 font-weight-light mb-0">+56 9 5535 6490</p>
+                                                        </a>
+                                                    </div>
+                                                </div>
+
+
+
+                                                <div class="col-lg-3 col-mb-6 mb-2">
+                                                
+                                                    <div data-plugin-float-element data-plugin-options="{'startPos': 'bottom', 'speed': 0.2, 'isInsideSVG': true, 'transition': true, 'transitionDuration': 2000}">
+                                                    <a href="https://api.whatsapp.com/send?phone=56984038643&amp;text=Hola%20Gasfiter%C3%ADa%20Metropolitana,%20quisiera%20comunicarme%20con%20ustedes." class="float" target="_blank">
+                                                        <i class="fab fa-whatsapp fa-4x" style="color:#47a43c; padding-bottom: 15px;"></i> 
+
+                                                        <h4 class="font-weight-bold text-color-light text-5 mb-0">Whatsapp</h4>
+                                                        <p class="text-3 text-color-light opacity-5 font-weight-light mb-0">Ignacio</p>
+                                                        </a>  
+                                                    </div>
+                                                </div>
+
+
+                                                <div class="col-lg-3 col-mb-6 mb-2">
+                                                
+                                                    <div data-plugin-float-element data-plugin-options="{'startPos': 'bottom', 'speed': 0.2, 'isInsideSVG': true, 'transition': true, 'transitionDuration': 2000}">
+                                                        <a href="https://api.whatsapp.com/send?phone=56955356490&amp;text=Hola%20Gasfiter%C3%ADa%20Metropolitana,%20quisiera%20comunicarme%20con%20ustedes." class="float" target="_blank">
+                                                        <i class="fab fa-whatsapp fa-4x" style="color:#47a43c; padding-bottom: 15px;"></i> 
+                                                        <h4 class="font-weight-bold text-color-light text-5 mb-0">Whatsapp</h4>
+                                                        <p class="text-3 text-color-light opacity-5 font-weight-light mb-0">Osvaldo</p>
+                                                    </a> 
+                                                    </div>
+                                                </div>
+											
+                                        </div>
+
+                                        <div class="row">
+                                            
+                                            <div class="col">
+												<div data-plugin-float-element data-plugin-options="{'startPos': 'bottom', 'speed': 0.1, 'isInsideSVG': true, 'transition': true, 'transitionDuration': 2000}">
+													<img class="icon-animated" width="55" src="img/icons/icon-calendar.svg" alt="" data-icon data-plugin-options="{'onlySVG': true, 'extraClass': 'svg-fill-color-primary mb-3'}" />
+													<h4 class="font-weight-bold text-color-light text-5 mb-0">Horario 24hrs</h4>
+													<p class="text-3 text-color-light opacity-5 font-weight-light mb-0">Lunes - Domingo</p>
+												</div>
+                                            </div>
+                                            
+										</div>
+
+                                    </div>
+                                    
+								</div>
+							</Div>
+
+						</div>
+					</div>
+				</div>
+				<div class="footer-copyright bg-dark mt-1 pb-5">
+					<div class="container footer-top-light-border pt-4">
+						<div class="row">
+							<div class="col">
+								<p class="text-center text-color-light opacity-5">Derechos Reservados © 2020 - Desarrollado por LetsCode.cl</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
